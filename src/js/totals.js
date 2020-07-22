1691a6e260e9cd4eda72cbd21039ecfa011159ee
Guilherme Moraes <guilherme.moraes@ifood.com.br>
2020-07-22 19:13:29 -0300

    Fix totals row alignment

--- src/js/totals.js
+++ src/js/totals.js
@@ -1,73 +1,73 @@
 const totalsHtml = {
     createHtml: function (values) {
-        return `<div class="sortableTableWrapper">
-    <table class="sortableTable js-statsTable">
-        <thead class="sortableTableHeaders">
-            <tr>
-                <th class="sortableTable-header sortableTable-title u-maxWidth450">
-                    <button class="button button--chromeless u-baseColor--buttonNormal">Totals</button>
-                </th>
-                <th class="sortableTable-header">
-                    <button class="button button--chromeless u-baseColor--buttonNormal">Views</button>
-                </th>
-                <th class="sortableTable-header">
-                    <button class="button button--chromeless u-baseColor--buttonNormal">Reads</button>
-                </th>
-                <th class="sortableTable-header">
-                    <button class="button button--chromeless u-baseColor--buttonNormal">Read ratio</button>
-                </th>
-                <th class="sortableTable-header">
-                    <button class="button button--chromeless u-baseColor--buttonNormal">Claps</button>
-                </th>
-                <th class="sortableTable-header">
-                    <button class="button button--chromeless u-baseColor--buttonNormal">Fans</button>
-                </th>
-                <th class="sortableTable-header">
-                    <button class="button button--chromeless u-baseColor--buttonNormal">Claps ratio</button>
-                </th>
-            </tr>
-        </thead>
-        <tbody class="js-statsTableBody">
-            <tr class="sortableTable-row js-statsTableRow">
-                <td>
-                    <div class="sortableTable-number" style="text-align: left">
-                        ${values.totals} articles
-                    </div>
-                </td>
-                <td>
-                    <span class="sortableTable-number">${values.views}
-                        <span class="u-sm-show"><br>views</span>
-                    </span>
-                </td>
-                <td>
-                    <span class="sortableTable-number">${values.reads}
-                        <span class="u-sm-show"><br>reads</span>
-                    </span>
-                </td>
-                <td>
-                    <span class="sortableTable-number">${getRatio(values.views, values.reads)}%
-                        <span class="u-sm-show"><br>ratio</span>
-                    </span>
-                </td>
-                <td>
-                    <span class="sortableTable-number">${values.claps}
-                        <span class="u-sm-show"><br>claps</span>
-                    </span>
-                </td>
-                <td>
-                    <span class="sortableTable-number">${values.fans}
-                        <span class="u-sm-show"><br>fans</span>
-                    </span>
-                </td>
-                <td>
-                    <span class="sortableTable-number">${values.fans ? (values.claps/values.fans).toFixed(1) : 0}
-                        <span class="u-sm-show"><br>claps ratio</span>
-                    </span>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>`
+        return `<div class="sortableTableWrapper total-table-wrapper">
+                    <table class="sortableTable js-statsTable totals-table">
+                        <thead class="sortableTableHeaders totals-table-header">
+                            <tr>
+                                <th class="sortableTable-header sortableTable-title u-maxWidth450">
+                                    <button class="button button--chromeless u-baseColor--buttonNormal">Totals</button>
+                                </th>
+                                <th class="sortableTable-header">
+                                    <button class="button button--chromeless u-baseColor--buttonNormal">Views</button>
+                                </th>
+                                <th class="sortableTable-header">
+                                    <button class="button button--chromeless u-baseColor--buttonNormal">Reads</button>
+                                </th>
+                                <th class="sortableTable-header">
+                                    <button class="button button--chromeless u-baseColor--buttonNormal">Read ratio</button>
+                                </th>
+                                <th class="sortableTable-header">
+                                    <button class="button button--chromeless u-baseColor--buttonNormal">Claps</button>
+                                </th>
+                                <th class="sortableTable-header">
+                                    <button class="button button--chromeless u-baseColor--buttonNormal">Fans</button>
+                                </th>
+                                <th class="sortableTable-header">
+                                    <button class="button button--chromeless u-baseColor--buttonNormal">Claps ratio</button>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="totals-table-body">
+                            <tr class="sortableTable-row js-statsTableRow">
+                                <td>
+                                    <div class="sortableTable-number" style="text-align: left">
+                                        ${values.totals} articles
+                                    </div>
+                                </td>
+                                <td>
+                                    <span class="sortableTable-number">${values.views}
+                                        <span class="u-sm-show"><br>views</span>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="sortableTable-number">${values.reads}
+                                        <span class="u-sm-show"><br>reads</span>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="sortableTable-number">${getRatio(values.views, values.reads)}%
+                                        <span class="u-sm-show"><br>ratio</span>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="sortableTable-number">${values.claps}
+                                        <span class="u-sm-show"><br>claps</span>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="sortableTable-number">${values.fans}
+                                        <span class="u-sm-show"><br>fans</span>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="sortableTable-number">${values.fans ? (values.claps / values.fans).toFixed(1) : 0}
+                                        <span class="u-sm-show"><br>claps ratio</span>
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>`
     }
 }
 
